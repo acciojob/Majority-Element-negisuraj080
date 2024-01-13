37064eef36d5cc7ec51eb2c6969603c890912cc9
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2024-01-13 20:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+function majorityElement(nums) {
+    // Initialize variables to keep track of the majority element and its count
+    let majorityElement = nums[0];
+    let count = 1;
+
+    // Loop through the array starting from the second element
+    for (let i = 1; i < nums.length; i++) {
+        // If the count is zero, update the majority element
+        if (count === 0) {
+            majorityElement = nums[i];
+            count = 1;
+        } else if (nums[i] === majorityElement) {
+            // If the current element is the majority element, increment the count
+            count++;
+        } else {
+            // If the current element is not the majority element, decrement the count
+            count--;
+        }
+    }
+
+    return majorityElement;
+}
